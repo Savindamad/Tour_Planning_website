37b6fb6323fb1fb1a6be5281291a59107433254d
savinda <savindamaddd@gmail.com>
2017-01-29 21:00:59 +0530

    updated

--- application/controllers/PackageController.php
+++ application/controllers/PackageController.php
@@ -4,6 +4,19 @@ class PackageController extends CI_Controller {
 
     public function index($param) {
         $this->load->model('PackageModel');
-        $this->load->view('packagePage');
+        $data["packageInfo"] = $this->PackageModel->getPackage($param);
+        $places = $this->PackageModel->getPlaces($param);
+        $allHotels = array();
+        $allActivities = array();
+        foreach ($places->result() as $row){
+            $hotels = $this->PackageModel->getHotels($row->place_id);
+            $activities = $this->PackageModel->getActivities($row->place_id);
+            $allHotels[] = $hotels;
+            $allActivities[] = $activities;
+        }
+        $data["places"] = $places;
+        $data["hotels"] = $allHotels;
+        $data["activities"] = $allActivities;
+        $this->load->view('packagePage',$data);
     }
 }
