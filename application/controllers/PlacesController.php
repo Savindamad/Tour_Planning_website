04519cf6de4b4672768f7751e1e53bb05e460d4d
savinda <savindamaddd@gmail.com>
2017-02-14 07:05:17 +0530

    complete places part

--- application/controllers/PlacesController.php
+++ application/controllers/PlacesController.php
@@ -13,7 +13,7 @@ class PlacesController extends CI_Controller {
         $name = $this->input->post('searchName');
         $this->load->model('PlacesModel');
         $array = $this->PlacesModel->searchPlaces($name);
-        
+
         echo '<div id="placeResult">
                 <div class="row" style="margin-top: 40px">
                     <div class="col-md-1"></div>
@@ -22,8 +22,7 @@ class PlacesController extends CI_Controller {
                             <h2>Search Result</h2>';
         if (sizeof($array->result()) > 0) {
             echo "<p>" . sizeof($array->result()) . " results found</p>";
-        } 
-        else {
+        } else {
             echo '<p>No result found</p>';
         }
         echo '</div>
@@ -51,7 +50,52 @@ class PlacesController extends CI_Controller {
         $this->load->model('PlacesModel');
         $main_places = $this->PlacesModel->getAllMainPlaces();
         $other_places = $this->PlacesModel->getRandOtherPlaces();
-        $this->load->view('placesPage', $data);
+
+        echo '<div class="row" style="margin-top: 40px">
+                    <div class="col-md-1"></div>
+                    <div class="col-md-10">
+                        <div class="content_white" style="text-align: left">
+                            <h2>Most popular places in Sri Lanka</h2>
+                            <p>Description</p>
+                        </div>
+                        <div class="row">';
+
+        foreach ($main_places->result() as $row) {
+
+            echo "<div class='col-md-3'>
+                                    <div class='thumbnail'>
+                                        <h3>$row->name</h3>
+                                        <img alt='Bootstrap Thumbnail First' src='base_url($row->image)' />
+                                        <div class='caption'>
+                                            <p>$row->description</p>
+                                            <p style='margin-top: 5px'><a class='btn btn-primary' href='".base_url('Place/') . $row->id."'>View more details</a></p>
+                                        </div>
+                                    </div>
+                                </div> ";
+        }
+        echo '</div>
+                        <div class="content_white" style="text-align: left">
+                            <h2>Other places in Sri Lanka</h2>
+                            <p>Description</p>
+                        </div>
+                        <div class="row">';
+        foreach ($other_places->result() as $row) {
+            echo "<div class='col-md-3'>
+                                    <div class='thumbnail'>
+                                        <h3>$row->name</h3>
+                                        <img alt='Bootstrap Thumbnail First' src='base_url($row->image)' />
+                                        <div class='caption'>
+                                            <p>$row->description</p>
+                                            <p style='margin-top: 5px'><a class='btn btn-primary' href='" . base_url('Place/') . $row->id . "'>View more details</a></p>
+                                        </div>
+                                    </div>
+                                </div> ";
+        }
+        echo '</div>
+                    </div>
+                    <div class="col-md-12">
+                    </div>
+                </div>';
     }
 
 }
--- public/js/searchPlaces.js
+++ public/js/searchPlaces.js
@@ -1,12 +1,25 @@
 function searchPlace(placeName) {
-    $.ajax({
-        url: 'PlacesController/searchPlace',
-        type: 'POST',
-        data: {
-            searchName: placeName
-        },
-        success: function (responseText) {
-            $('#placeResult').html(responseText);
-        }
-    });
+    if (placeName == "") {
+        $.ajax({
+            url: 'PlacesController/loadPlaces',
+            type: 'POST',
+            data: {
+                searchName: placeName
+            },
+            success: function (responseText) {
+                $('#placeResult').html(responseText);
+            }
+        });
+    } else {
+        $.ajax({
+            url: 'PlacesController/searchPlace',
+            type: 'POST',
+            data: {
+                searchName: placeName
+            },
+            success: function (responseText) {
+                $('#placeResult').html(responseText);
+            }
+        });
+    }
 }
