9801eabe70ee5b5494b497f97898a159ba4856dc
savinda <savindamaddd@gmail.com>
2017-02-21 11:45:41 +0530

    test 3

--- application/views/homePage.php
+++ application/views/homePage.php
@@ -20,6 +20,31 @@
         $this->load->view('website/navbarNw');
         ?>
         <header id="myCarousel" class="carousel slide" style="margin-top: 100px">
+            <div class="cd-logo"><a href="#0"><img src="img/cd-logo.svg" alt="Logo"></a></div>
+
+            <nav class="cd-main-nav-wrapper">
+                <ul class="cd-main-nav">
+                    <li><a href="#0">About</a></li>
+                    <li><a href="#0">Projects</a></li>
+                    <li><a href="#0">Blog</a></li>
+                    <li><a href="#0">Contact</a></li>
+                    <li>
+                        <a href="#0" class="cd-subnav-trigger"><span>Categories</span></a>
+
+                        <ul>
+                            <li class="go-back"><a href="#0">Menu</a></li>
+                            <li><a href="#0">Category 1</a></li>
+                            <li><a href="#0">Category 2</a></li>
+                            <li><a href="#0">Category 3</a></li>
+                            <li><a href="#0">Category 4</a></li>
+                            <li><a href="#0">Category 5</a></li>
+                            <li><a href="#0" class="placeholder">Placeholder</a></li>
+                        </ul>
+                    </li>
+                </ul> <!-- .cd-main-nav -->
+            </nav> <!-- .cd-main-nav-wrapper -->
+
+            <a href="#0" class="cd-nav-trigger">Menu<span></span></a>
 
             <!-- Wrapper for Slides -->
             <div class="carousel-inner">
