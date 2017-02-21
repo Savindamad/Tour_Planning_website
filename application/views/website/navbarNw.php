cffa2f70d5d3308cf46a9555f8182b9d58b0b8cb
savinda <savindamaddd@gmail.com>
2017-02-21 15:36:55 +0530

    updated

--- application/views/website/navbarNw.php
+++ application/views/website/navbarNw.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+<nav class="navbar navbar-fixed-top" role="navigation">
     <div class="container">
         <!-- Brand and toggle get grouped for better mobile display -->
         <div class="navbar-header">
@@ -9,12 +9,12 @@
                 <span class="icon-bar"></span>
             </button>
             <a class="navbar-brand" href="#">
-                <img style="width: 200px" src="<?php echo base_url('public/images/logo1.png'); ?>" alt="">
+                <img src="http://placehold.it/150x50&text=Logo" alt="">
             </a>
         </div>
         <!-- Collect the nav links, forms, and other content for toggling -->
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-            <ul class="nav navbar-nav">
+            <ul class="nav navbar-nav left">
                 <li>
                     <a href="#">About</a>
                 </li>
