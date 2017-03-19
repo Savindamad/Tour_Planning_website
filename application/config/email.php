713f6bac741dd5efc44a9f30db7642e55398dd96
savinda <savindamaddd@gmail.com>
2017-03-19 06:19:40 +0530

    test 5

---
+++ application/config/email.php
@@ -0,0 +1,8 @@
+<?php
+
+$config['protocol'] = 'sendmail';
+$config['mailpath'] = '/usr/sbin/sendmail';
+$config['charset'] = 'iso-8859-1';
+$config['wordwrap'] = TRUE;
+
+$this->email->initialize($config);
